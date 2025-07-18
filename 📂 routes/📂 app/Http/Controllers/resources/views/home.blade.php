934d01016c27c10cfae600d867e3e907be954bb3
Jaya Deepthi M <166888904+Jayadeepthideepu@users.noreply.github.com>
2025-07-18 16:22:44 +0530

    Create home.blade.php

---
+++ 📂 routes/📂 app/Http/Controllers/resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kanha Furniture</title>
+</head>
+<body>
+    <h1>Welcome to Kanha Furniture Store</h1>
+    <p>Explore our collection of products.</p>
+</body>
+</html>
