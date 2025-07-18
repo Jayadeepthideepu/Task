676718cba9a23d64d23271dd8dd6cb1e96ef7d55
Jaya Deepthi M <166888904+Jayadeepthideepu@users.noreply.github.com>
2025-07-18 16:23:06 +0530

    Create product.blade.php

---
+++ 📂 routes/📂 app/Http/Controllers/resources/views/resources/views/product.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product Page</title>
+</head>
+<body>
+    <h1>Product Details: {{ $slug }}</h1>
+    <p>Description and price here.</p>
+</body>
+</html>
